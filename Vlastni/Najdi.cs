a572e43f0943bb544bd3b1bfeeaf260f6adaf0da
Jakub Hutař <hutarj.04@spst.cz>
2022-05-17 17:04:00 +0200

    Přidejte soubory projektu.

---
+++ Vlastni/Najdi.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vlastni
+{
+    public class Najdi
+    {
+        public static double DruheMax(List<double> cisla)
+        {
+            if (cisla.Count > 0)
+            {
+                double druheMax = double.MinValue;
+                double max = double.MinValue;
+                foreach (double prochazeneCislo in cisla)
+                {
+                    if (prochazeneCislo > max)
+                    {
+                        druheMax = max;
+                        max = prochazeneCislo;
+                    }
+                    else if (prochazeneCislo > druheMax) druheMax = prochazeneCislo;
+                }
+                return druheMax;
+            }
+            else return -1;
+        }
+        public static double DruheMin(List<double> cisla)
+        {
+            if (cisla.Count > 0)
+            {
+                double druheMin = double.MaxValue;
+                double min = double.MaxValue;
+                foreach (double prochazeneCislo in cisla)
+                {
+                    if (prochazeneCislo < druheMin)
+                    {
+                        druheMin = min;
+                        min = prochazeneCislo;
+                    }
+                    else if (prochazeneCislo > druheMin) druheMin = prochazeneCislo;
+                }
+                return druheMin;
+            }
+            else return -1;
+        }
+
+        public static char znakvNejvetsimPoctu(List<char> znaky)
+        {
+            char znakMax = ' ';
+            int pocetMax = -1;
+            int pocet = 0;
+
+            foreach (char i in znaky)
+            {
+                foreach (char j in znaky)
+                {
+                    if (i == j) ++pocet;
+                }
+
+                if (pocet > pocetMax)
+                {
+                    znakMax = i;
+                    pocetMax = pocet;
+                }
+                pocet = 0;
+            }
+
+            return znakMax;
+        }
+    }
+}
---
+++ VlastniKnihovna/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vlastni;
+
+namespace VlastniKnihovna
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<double> cisla = new List<double>();
+            double x;
+            foreach (string i in textBox1.Lines)
+            {
+                x = double.Parse(i);
+                cisla.Add(x);
+            }
+
+            MessageBox.Show("Druhé maximum je " + Najdi.DruheMax(cisla) + "\nDruhé minimum je " + Najdi.DruheMin(cisla));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<char> znaky = textBox2.Text.ToList();
+
+            MessageBox.Show("Znak v největším počtu je " + Najdi.znakvNejvetsimPoctu(znaky));
+        }
+    }
+            
+}
